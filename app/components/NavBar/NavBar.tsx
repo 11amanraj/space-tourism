95bf7d3fcb46aed8aa19b9673bbb1f0bfdbe56a2
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-03-29 00:17:55 +0530

    created interfaces file

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import NavElement from './NavElement'
+import { NavbarElement } from '@/app/interfaces/interfaces'
 
 const NavBar = () => {
   return (
-    <div>
-      <NavElement />
-    </div>
+    <nav className='flex items-center justify-between'>
+      <div>LOGO</div>
+      <div>
+        <NavElement />
+        <NavElement />
+        <NavElement />
+        <NavElement />
+      </div>
+    </nav>
   )
 }
 
---
+++ app/interfaces/interfaces.ts
@@ -0,0 +1,5 @@
+export interface NavbarElement {
+    number: number,
+    title: string,
+    link: string
+  }
