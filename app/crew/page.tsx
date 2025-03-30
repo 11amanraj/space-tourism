2e3e34a781e068650adbe4ed14b6931f7f6a6504
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-03-31 01:27:50 +0530

    added crew details

--- app/crew/page.tsx
+++ app/crew/page.tsx
@@ -1,22 +1,44 @@
 import React from 'react'
 
 const page = () => {
+    const crew = [
+        {
+            name: 'Commander',
+            role: 'Douglas Hurley',
+            description: 'Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.'
+        },
+        {
+            name: 'Mission Specialist',
+            role: 'MARK SHUTTLEWORTH',
+            description: 'Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.'
+        },
+        {
+            name: 'PILOT',
+            role: 'Victor Glover',
+            description: 'Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.'
+        },
+        {
+            name: 'Flight Engineer',
+            role: 'Anousheh Ansari',
+            description: 'Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.'
+        }   
+    ]
   return (
-    <div>
+    <div className='p-6'>
         <div className='text-preset-8 flex gap-6 justify-center sm:text-[20px] sm:justify-start'>
             <p className='opacity-25'>02</p>
             <p>MEET YOUR CREW</p>
         </div>
-        <div className='flex flex-col'>
-            <div className='flex flex-col'>
+        <div className='flex flex-col pt-6 md:flex-row'>
+            <div className='flex-1 flex flex-col'> 
                 <div className='flex flex-col items-center'>
-                    <h3>Commander</h3>
-                    <h1>Douglas Hurley</h1>
-                    <p className='text-center'>Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.</p>
+                    <h3 className='pt-10 text-preset-4 text-[18px] opacity-50 sm:text-[24px]'>COMMANDER</h3>
+                    <h1 className='pt-2 text-preset-3 text-[24px] sm:text-[40px]'>DOUGLAS HURLEY</h1>
+                    <p className='pt-6 text-center text-preset-9 text-[15px] text-tertiary sm:text-[16px]'>Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.</p>
                 </div>
                 <div className='self-center'>Carousel</div>
             </div>
-            <div className='bg-tertiary w-70 h-85 self-center'>
+            <div className='flex-1 bg-tertiary w-70 h-85 self-center'>
                 Image Here
             </div>
         </div>
