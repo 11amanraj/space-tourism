993a1631e01bb7b14ee7c423b9eb70d2809a5a2a
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-03-31 15:01:19 +0530

    Added Logo and hamburger icon to NavBar component

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -28,8 +28,8 @@ const NavBar = () => {
 
   return (
     <nav className='fixed top-0 left-0 w-full flex items-center justify-between pt-10 pl-16'>
-      <div>LOGO</div>
-      <div className='block md:hidden'>hamburger</div>
+      <img src="/shared/logo.svg" alt="logo"  className='w-10 h-10 sm:w-12 sm:h-12'/>
+      <img src="/shared/icon-hamburger.svg" alt="hamburger" className='w-6 h-6 block md:hidden'/>
       <div className='hidden md:flex gap-12 pl-[12.5vw] pr-[5vw] bg-secondary/5 backdrop-blur-2xl'>
         {NavigationElement.map(item => 
           <NavElement title={item.title} index={item.index} link={item.link} key={item.index}/>
