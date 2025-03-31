f5ad6e49d8aab0701c3966f2af29086e52547916
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-03-31 15:11:04 +0530

    Added Links to Navbar

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -1,34 +1,37 @@
 import React from 'react'
 import NavElement from './NavElement'
 import { NavbarElement } from '@/app/interfaces/interfaces'
+import Link from 'next/link'
 
 const NavBar = () => {
   const NavigationElement : NavbarElement[] = [
     {
       index: 0,
       title: 'Home',
-      link: 'www.google.com'
+      link: '/'
     },
     {
       index: 1,
       title: 'Destination',
-      link: 'www.google.com'
+      link: '/destination'
     },
     {
       index: 2,
       title: 'Crew',
-      link: 'www.google.com'
+      link: '/crew'
     },
     {
       index: 3,
       title: 'Technology',
-      link: 'www.google.com'
+      link: '/technology'
     }
   ]
 
   return (
     <nav className='fixed top-0 left-0 w-full flex items-center justify-between pt-10 pl-16'>
-      <img src="/shared/logo.svg" alt="logo"  className='w-10 h-10 sm:w-12 sm:h-12'/>
+      <Link href={'/'}>
+        <img src="/shared/logo.svg" alt="logo"  className='w-10 h-10 sm:w-12 sm:h-12'/>
+      </Link>
       <img src="/shared/icon-hamburger.svg" alt="hamburger" className='w-6 h-6 block md:hidden'/>
       <div className='hidden md:flex gap-12 pl-[12.5vw] pr-[5vw] bg-secondary/5 backdrop-blur-2xl'>
         {NavigationElement.map(item => 
--- app/components/NavBar/NavElement.tsx
+++ app/components/NavBar/NavElement.tsx
@@ -1,13 +1,14 @@
 import { NavbarElement } from '@/app/interfaces/interfaces'
+import Link from 'next/link'
 import React from 'react'
 
 const NavElement: React.FC<NavbarElement> = ({index, title, link}) => {
   return (
-    <div className='cursor-pointer h-24 inline-flex items-center basis-auto hover:border-b-3 border-tertiary'>
-        <div className='text-preset-8 flex gap-3 text-secondary'>
-            <div className='font-bold'>{`0${index}`}</div>
-            <div>{title}</div>
-        </div>
+    <div className='outline-1 cursor-pointer h-24 inline-flex items-center basis-auto hover:border-b-3 border-tertiary'>
+      <Link href={link} className='outline-1 text-preset-8 flex gap-3 text-secondary'>
+        <div className='font-bold'>{`0${index}`}</div>
+        <div>{title}</div>
+      </Link>
     </div>
   )
 }
