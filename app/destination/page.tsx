fc6658c611e5b0f674a0bc6dc0e78efe9abe2a67
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-03-31 15:45:55 +0530

    Added background images to destination page

--- app/destination/page.tsx
+++ app/destination/page.tsx
@@ -6,29 +6,33 @@ const page = () => {
             name: 'MOON',
             description: 'See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.',
             distance: '384,440 KM',
-            travel_time: '3 DAYS'
+            travel_time: '3 DAYS',
+            image: '/destination/image-moon'
         },
         {
             name: 'MARS',
             description: 'Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!',
             distance: '225 MIL. KM',
-            travel_time: '9 MONTHS' 
+            travel_time: '9 MONTHS',
+            image: '/destination/image-mars' 
         },
         {
             name: 'EUROPA',
             description: 'The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.',
             distance: '628 MIL. KM',
-            travel_time: '3 YEARS '
+            travel_time: '3 YEARS',
+            image: '/destination/image-europa'
         },
         {
             name: 'TITAN',
             description: 'The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.',
             distance: '1.6 BIL. KM',
-            travel_time: '7 YEARS'
+            travel_time: '7 YEARS',
+            image: '/destination/image-titan'
         }
     ]
   return (
-    <div className='px-6 sm:p-10 md:py-12 md:px-40 h-full sm:bg-blue-700 md:bg-green-500'>
+    <div className="bg-[url('/destination/background-destination-mobile.jpg')] sm:bg-[url('/destination/background-destination-tablet.jpg')] md:bg-[url('/destination/background-destination-desktop.jpg')] bg-cover px-6 sm:p-10 md:py-12 md:px-40 h-full">
         <div className='text-preset-8 flex gap-6 justify-center sm:text-[20px] sm:justify-start'>
             <p className='opacity-25'>01</p>
             <p>PICK YOUR DESTINATION</p>
