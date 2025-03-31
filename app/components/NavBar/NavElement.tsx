ec28df72ddfa085de85ed7e7c2f3ce3d94301732
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-03-31 15:11:55 +0530

    minor changes to navelement component

--- app/components/NavBar/NavElement.tsx
+++ app/components/NavBar/NavElement.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 
 const NavElement: React.FC<NavbarElement> = ({index, title, link}) => {
   return (
-    <div className='outline-1 cursor-pointer h-24 inline-flex items-center basis-auto hover:border-b-3 border-tertiary'>
-      <Link href={link} className='outline-1 text-preset-8 flex gap-3 text-secondary'>
+    <div className='cursor-pointer h-24 inline-flex items-center basis-auto hover:border-b-3 border-tertiary'>
+      <Link href={link} className='text-preset-8 flex gap-3 text-secondary'>
         <div className='font-bold'>{`0${index}`}</div>
         <div>{title}</div>
       </Link>
