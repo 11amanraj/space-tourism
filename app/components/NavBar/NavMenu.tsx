1ec8f63cccade898057fe23b1ae7480e56fb202a
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-04-01 20:41:51 +0530

    stopped scrolling when drawer is open

--- app/components/NavBar/NavMenu.tsx
+++ app/components/NavBar/NavMenu.tsx
@@ -7,8 +7,16 @@ const NavMenu = () => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
 
     useEffect(() => {
-        console.log(isOpen)
-    }, [isOpen]);
+        if (isOpen) {
+          document.body.style.overflow = 'hidden';
+        } else {
+          document.body.style.overflow = '';
+        }
+      
+        return () => {
+          document.body.style.overflow = '';
+        };
+      }, [isOpen]);
 
   return (
     <>
